1fbc8984740ef406539ae5647d3924498fc126e7
Mariah <mhortillano@github.com>
2018-01-22 10:47:29 -1000

    Made Press Me Button

--- HackWhich2/ViewController.swift
+++ HackWhich2/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func PressMeButton(_ sender: Any) {
+        //code block
+          print("Hello World")
+    }
 }
 
